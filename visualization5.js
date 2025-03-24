ca0d5fdd64123c4390e254808f3c42b821296cd6
elissa-alarmani <elissa1327@gmail.com>
2025-03-24 19:35:21 -0400

    change map tile and format

--- visualization5.js
+++ visualization5.js
@@ -1,11 +1,18 @@
 // Create a Leaflet map centered on Boston
 const map = L.map("map").setView([42.36, -71.06], 13);
 
-// Add OpenStreetMap tile layer to the map
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+// Add Jawg.Sunny tile layer to the map
+L.tileLayer(
+  "https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token=HYMk9xl71FLXhzPodW32GowAavigUQJldDTriyp50JsWrq5sTzaMrLQI6QvC22PQ",
+  {
+    attribution:
+      '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: 0,
+    maxZoom: 22,
+    accessToken:
+      "HYMk9xl71FLXhzPodW32GowAavigUQJldDTriyp50JsWrq5sTzaMrLQI6QvC22PQ",
+  }
+).addTo(map);
 
 // Create a D3 ordinal scale to assign colors to different neighborhoods
 const colorScale = d3.scaleOrdinal().range(d3.schemeSet1);
@@ -27,17 +34,16 @@ d3.csv("rodent_data_with_neighborhoods.csv")
     // For each individual case, create a circle marker on the map
     data.forEach((d) => {
       const color = colorScale(d.neighborhood);
-      // Create a circle marker with Leaflet for each case
       const marker = L.circleMarker([d.latitude, d.longitude], {
-        radius: 5,
+        radius: 3,
         fillColor: color,
         color: color,
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.7,
+        fillOpacity: 0.5,
       }).addTo(map);
 
-      // Bind a popup to display case details when the marker is clicked
+      // Bind a popup
       marker.bindPopup(
         `<strong>Case ID:</strong> ${d.case_enquiry_id}<br/>
          <strong>Neighborhood:</strong> ${d.neighborhood}<br/>
